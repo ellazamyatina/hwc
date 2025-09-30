c84386166432afcac9547b4b0628eb21278a6794
Ella Zamyatina <ellazamyatina567@gmail.com>
2025-09-30 22:27:30 +0300

    Deleted 'unsigned' and initalized variables

--- src/hw1/division.c
+++ src/hw1/division.c
@@ -7,14 +7,14 @@ int integer_division(int a, int b) {
     }
     
     int sign = 1;
-    if ((a < 0 && b > 0) || (a > 0 && b < 0)) {
+    if ((a * b) < 0) {
         sign = -1;
     }
     
-    unsigned int ua = (a < 0) ? -a : a;
-    unsigned int ub = (b < 0) ? -b : b;
-    
-    unsigned int quotient = 0;
+    int ua = (a < 0) ? -a : a;
+    int ub = (b < 0) ? -b : b;
+    int quotient = 0;
+
     while (ua >= ub) {
         ua -= ub;
         quotient++;
@@ -24,11 +24,14 @@ int integer_division(int a, int b) {
 }
 
 int main() {
-    int a, b;
+    int a = 0;
+    int b = 0;
+
     printf("Введите a и b: ");
     scanf("%d %d", &a, &b);
     
     int result = integer_division(a, b);
+
     printf("Неполное частное: %d\n", result);
     
     return 0;
