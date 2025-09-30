1e639b84f539b41b28c264f6db7232f703519a04
Ella Zamyatina <ellazamyatina567@gmail.com>
2025-09-30 22:05:07 +0300

    Initialized variables

--- src/hw1/2_multiplications.c
+++ src/hw1/2_multiplications.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
-
-double polynomial (double x) {
-    double p = x * x;           
-    double t = (p + 1) * (p + x); 
-    return t + 1;
-}
+#include <math.h>
 
 int main() {
-    double x;
-    printf("Введите x: ");
-    scanf("%lf", &x);
-    printf("%.2f\n", polynomial(x));
+    int x = 0;        
+    int result = 0;   
+    int x2 = 0;       
+    int tempVar1 = 0;    
+    int tempVar2 = 0;    
+    int tempVar3 = 0;    
+    
+    printf("Введите значение x: ");
+    scanf("%d", &x);
+    
+    x2 = pow(x,2);
+    
+    tempVar1 = x2 + x;   
+    tempVar2 = x2 + 1;   
+    tempVar3 = tempVar1 * tempVar2;
+    
+    result = tempVar3 + 1;
+    
+    printf("Результат: %d\n", result);
+    
     return 0;
 }
