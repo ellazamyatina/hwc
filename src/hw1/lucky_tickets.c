c65b509fdddf76209a8ba649ccfa5cbd701a4d2b
Ella Zamyatina <ellazamyatina567@gmail.com>
2025-09-30 22:12:08 +0300

    Remade code(using 'for loop' instead of functions and initalized each of variables)

--- src/hw1/lucky_tickets.c
+++ src/hw1/lucky_tickets.c
@@ -1,29 +1,41 @@
 #include <stdio.h>
 
 int main() {
+    int sum3 = 0;        // sum of 3 num
+    int count = 0;       
+    int num = 0;         // num of the ticket
+    int first_part = 0;  // first 3 num of ticket
+    int second_part = 0; // last 3 num of ticket
+    int sumOf1Part = 0;  
+    int sumOf2Part = 0;  
+    int digit1 = 0;      // first num of ticket's num
+    int digit2 = 0;      
+    int digit3 = 0;     
+    int digit4 = 0;     
+    int digit5 = 0;     
+    int digit6 = 0;     
+    
+    printf("Введите сумму трех цифр: ");
+    scanf("%d", &sum3);
+    
+    for(num = 0; num <= 999999; num++) {
 
-    int count[28] = {0}; 
-
-    for (int num = 0; num < 1000; num++) {
-        int sum = (num / 100) + ((num / 10) % 10) + (num % 10);
-        count[sum]++;
+        digit1 = num / 100000;
+        digit2 = (num / 10000) % 10;
+        digit3 = (num / 1000) % 10;
+        digit4 = (num / 100) % 10;
+        digit5 = (num / 10) % 10;
+        digit6 = num % 10;
+        
+        sumOf1Part = digit1 + digit2 + digit3;
+        sumOf2Part = digit4 + digit5 + digit6;
+        
+        if(sumOf1Part == sum3 && sumOf2Part == sum3) {
+            count++;
+        }
     }
     
-    int target_sum;
-    printf("Введите сумму трех цифр (0-27): ");
-    scanf("%d", &target_sum);
-    
-    if (target_sum < 0 || target_sum > 27) {
-        printf("Ошибка: сумма должна быть от 0 до 27\n");
-        return 1;
-    }
-    
-    int tickets_with_sum = count[target_sum];
-    
-    
-    int lucky_tickets = count[target_sum] * count[target_sum];
-    
-    printf("Счастливых билетов с суммой %d: %d\n", target_sum, lucky_tickets);
+    printf("Количество счастливых билетов с суммой %d: %d\n", sum3, count);
     
     return 0;
 }
