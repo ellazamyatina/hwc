52eefaec8b37a6f4d762e305b5e9efb2d55bb643
Ella Zamyatina <ellazamyatina567@gmail.com>
2025-10-13 14:46:10 +0300

    Add program calculating sum of even fibs

---
+++ src/test1/sumEvenFib.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+  int limit = 1000000;
+  int a = 1; // last num
+  int b = 1; // current fib
+  int tempVar = 0;
+  int sum = 0;
+
+  while (b <= limit) {
+    if (b % 2 == 0) {
+      sum += b;
+    }
+
+    // calculate the next fib
+    tempVar = a + b;
+    a = b;
+    b = tempVar;
+  }
+
+  printf("Сумма всех чётных чисел Фибоначчи до 1000000: %lld\n", sum);
+
+  return 0;
+}
