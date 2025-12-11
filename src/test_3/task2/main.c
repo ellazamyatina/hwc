42d5ba09a5530e67b42a98297eb82428e8628cc0
Ella Zamyatina <ellazamyatina567@gmail.com>
2025-12-11 17:02:10 +0300

    Implement replacement starting with the smallest digit

---
+++ src/test_3/task2/main.c
@@ -0,0 +1,15 @@
+#include "replaceByMin.h"
+#include <stdio.h>
+
+int main(void) {
+  char s[100];
+
+  printf("Введите число: ");
+  if (scanf("%99s", s) != 1)
+    return 1;
+
+  makeMinNmber(s);
+
+  printf("Наименьшее число: %s\n", s);
+  return 0;
+}
