82627100aad1f6e7a2b331485faa4906d88d017d
Ella Zamyatina <ellazamyatina567@gmail.com>
2025-12-20 01:23:20 +0300

    Make for cycle instead of while

--- hw2/bracketBalance.c
+++ hw2/bracketBalance.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
-  char input[1000] = "\0";
-  int balance = 0;
-  printf("Введите строку для проверки баланса скобок: ");
-  fgets(input, sizeof(input), stdin);
-  input[strlen(input) - 1] = '\0';
+int main()
+{
+    char input[1000] = "\0";
+    int balance = 0;
+    printf("Введите строку для проверки баланса скобок: ");
+    fgets(input, sizeof(input), stdin);
+    input[strlen(input) - 1] = '\0';
 
-  for (int i = 0; input[i] != '\0'; i++) {
-    if (input[i] == '(') {
-      balance++;
-    } else if (input[i] == ')') {
-      balance--;
+    for (int i = 0; input[i] != '\0'; i++) {
+        if (input[i] == '(') {
+            balance++;
+        } else if (input[i] == ')') {
+            balance--;
 
-      if (balance < 0) {
-        break;
-      }
+            if (balance < 0) {
+                break;
+            }
+        }
     }
-  }
 
-  if (balance == 0) {
-    printf("Скобки сбалансированы\n");
-  } else {
-    printf("Скобки НЕ сбалансированы\n");
-  }
+    if (balance == 0) {
+        printf("Скобки сбалансированы\n");
+    } else {
+        printf("Скобки НЕ сбалансированы\n");
+    }
 
-  return 0;
+    return 0;
 }
