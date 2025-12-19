085b9f96afd29b8407017cc68516d08502451856
Ella Zamyatina <ellazamyatina567@gmail.com>
2025-12-19 13:28:25 +0300

    Delete temp vars and remake structure of the code

--- src/hw1/2_multiplications.c
+++ src/hw1/2_multiplications.c
@@ -1,26 +1,15 @@
 #include <stdio.h>
-#include <math.h>
 
 int main() {
-    int x = 0;        
-    int result = 0;   
-    int x2 = 0;       
-    int tempVar1 = 0;    
-    int tempVar2 = 0;    
-    int tempVar3 = 0;    
-    
-    printf("Введите значение x: ");
-    scanf("%d", &x);
-    
-    x2 = pow(x,2);
-    
-    tempVar1 = x2 + x;   
-    tempVar2 = x2 + 1;   
-    tempVar3 = tempVar1 * tempVar2;
-    
-    result = tempVar3 + 1;
-    
-    printf("Результат: %d\n", result);
-    
-    return 0;
+  int x = 0;
+
+  printf("Введите значение x: ");
+  scanf("%d", &x);
+
+  int t = x * x;
+  int result = (t + x) * (t + 1) + 1;
+
+  printf("Результат: %d\n", result);
+
+  return 0;
 }
