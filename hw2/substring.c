b546edc022f116aece093e42b43658833e61d228
Ella Zamyatina <ellazamyatina567@gmail.com>
2025-12-20 01:21:25 +0300

    Make for cycle instead of while

--- hw2/substring.c
+++ hw2/substring.c
@@ -1,32 +1,30 @@
 #include <stdio.h>
 
-int main() {
-  char str[100] = "\0";
-  char sub[100] = "\0";
-  int count = 0;
-  int i = 0;
+int main()
+{
+    char str[100] = "\0";
+    char sub[100] = "\0";
+    int count = 0;
 
-  printf("Введите основную строку: ");
-  scanf("%s", &str);
+    printf("Введите основную строку: ");
+    scanf("%s", &str);
 
-  printf("Введите подстроку: ");
-  scanf("%s", &sub);
+    printf("Введите подстроку: ");
+    scanf("%s", &sub);
 
-  while (str[i] != '\0') {
-    int j = 0;
-    while (sub[j] != '\0') {
-      if (str[i + j] != sub[j]) {
-        break;
-      }
-      j++;
+    for (int i = 0; str[i] != '\0'; i++) {
+        int j;
+        for (j = 0; sub[j] != '\0'; j++) {
+            if (str[i + j] != sub[j]) {
+                break;
+            }
+        }
+        if (sub[j] == '\0') {
+            count++;
+        }
     }
-    if (sub[j] == '\0') {
-      count++;
-    }
-    i++;
-  }
 
-  printf("Количество вхождений: %d\n", count);
+    printf("Количество вхождений: %d\n", count);
 
-  return 0;
+    return 0;
 }
