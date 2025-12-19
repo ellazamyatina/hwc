38161d31fb9379f2fe8b51834d6e3aa7637625f2
Ella Zamyatina <ellazamyatina567@gmail.com>
2025-12-20 01:22:20 +0300

    Initialize array

--- hw2/zerosInArray.c
+++ hw2/zerosInArray.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 
-int main() {
-  int n = 0;
-  int count = 0;
+int main()
+{
+    int n = 0;
+    int count = 0;
 
-  printf("Введите количество элементов в массиве: ");
-  scanf("%d", &n);
+    printf("Введите количество элементов в массиве: ");
+    scanf("%d", &n);
 
-  int arr[n];
+    int arr[n] = {};
 
-  printf("Введите элементы массива:\n");
-  for (int i = 0; i < n; i++) {
-    scanf("%d", &arr[i]);
-  }
-
-  for (int i = 0; i < n; i++) {
-    if (arr[i] == 0) {
-      count++;
+    printf("Введите элементы массива:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
-  }
 
-  printf("Количество нулевых элементов: %d\n", count);
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == 0) {
+            count++;
+        }
+    }
 
-  return 0;
+    printf("Количество нулевых элементов: %d\n", count);
+
+    return 0;
 }
