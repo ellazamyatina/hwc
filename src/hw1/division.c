eabcdbda4d9fe83f7b11cf548a089f15eb4f3ff1
Ella Zamyatina <ellazamyatina567@gmail.com>
2025-12-20 01:46:28 +0300

    make webkit

--- src/hw1/division.c
+++ src/hw1/division.c
@@ -1,38 +1,40 @@
 #include <stdio.h>
 
-int integer_division(int a, int b) {
-  if (b == 0) {
-    printf("Ошибка: деление на ноль\n");
-    return 0;
-  }
+int integer_division(int a, int b)
+{
+    if (b == 0) {
+        printf("Ошибка: деление на ноль\n");
+        return 0;
+    }
 
-  int sign = 1;
-  if ((a * b) < 0) {
-    sign = -1;
-  }
+    int sign = 1;
+    if ((a * b) < 0) {
+        sign = -1;
+    }
 
-  int ua = (a < 0) ? -a : a;
-  int ub = (b < 0) ? -b : b;
-  int quotient = 0;
+    int ua = (a < 0) ? -a : a;
+    int ub = (b < 0) ? -b : b;
+    int quotient = 0;
 
-  while (ua >= ub) {
-    ua -= ub;
-    quotient++;
-  }
+    while (ua >= ub) {
+        ua -= ub;
+        quotient++;
+    }
 
-  return sign * quotient;
+    return sign * quotient;
 }
 
-int main() {
-  int a = 0;
-  int b = 0;
+int main()
+{
+    int a = 0;
+    int b = 0;
 
-  printf("Введите a и b: ");
-  scanf("%d %d", &a, &b);
+    printf("Введите a и b: ");
+    scanf("%d %d", &a, &b);
 
-  int result = integer_division(a, b);
+    int result = integer_division(a, b);
 
-  printf("Неполное частное: %d\n", result);
+    printf("Неполное частное: %d\n", result);
 
-  return 0;
+    return 0;
 }
