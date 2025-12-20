13dcab3ec5e2587bea032c9beffcb073d4b55fb6
Ella Zamyatina <ellazamyatina567@gmail.com>
2025-12-20 23:54:14 +0300

    Cmake for 2.2 task

---
+++ src/hw1/task2/array.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swapPartsOfArray(int* array, int left, int right)
+{
+    while (left < right) {
+        int temp = array[left];
+        array[left] = array[right];
+        array[right] = temp;
+        ++left;
+        --right;
+    }
+}
+
+int main()
+{
+    int m = 0;
+    int n = 0;
+
+    printf("Введите m и n: ");
+    scanf("%d %d", &m, &n);
+
+    int len = n + m;
+    int* array = malloc(len * sizeof(int));
+
+    printf("Введите %d элементов массива: ", len);
+    for (int i = 0; i < len; i++) {
+        scanf("%d", &array[i]);
+    }
+
+    printf("Исходный массив: ");
+    for (int i = 0; i < m + n; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+
+    swapPartsOfArray(array, 0, m - 1);
+    swapPartsOfArray(array, m, m + n - 1);
+    swapPartsOfArray(array, 0, m + n - 1);
+
+    printf("После обмена: ");
+    for (int i = 0; i < len; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+    free(array);
+    return 0;
+}
