df28036cfcd10c8d249c54ae26b55e03c2ade5df
Ella Zamyatina <ellazamyatina567@gmail.com>
2025-12-20 23:52:11 +0300

    Cmake for 2.1 task

---
+++ src/hw1/task1/2_multiplications.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int x = 0;
+
+    printf("Введите значение x: ");
+    scanf("%d", &x);
+
+    int t = x * x;
+    int result = (t + x) * (t + 1) + 1;
+
+    printf("Результат: %d\n", result);
+
+    return 0;
+}
