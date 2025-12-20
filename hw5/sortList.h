b9de4424b90841fcd484f4225c4dadbdbf9890ad
Ella Zamyatina <ellazamyatina567@gmail.com>
2025-12-20 21:15:53 +0300

    add errors handling

--- hw5/sortList.h
+++ hw5/sortList.h
@@ -1,14 +1,19 @@
 #pragma once
 
 typedef struct Node {
-  int data;
-  struct Node *next;
+    int data;
+    struct Node* next;
 } Node;
 
-// function for list
-Node *createNode(int value);
-void insertSorted(Node **head, int value);
+// for errors
+typedef enum { ok = 0,
+    error_memory,
+    error_not_found,
+    error_empty } ErrorCode;
 
-void deleteValue(Node **head, int value);
-void printList(Node *head);
-void freeList(Node *head);
+// function for list
+Node* createNode(int value, ErrorCode* error);
+ErrorCode insertSorted(Node** head, int value);
+ErrorCode deleteValue(Node** head, int value);
+void printList(Node* head);
+void freeList(Node* head);
