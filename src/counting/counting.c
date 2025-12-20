bfb5d97b6374141ae79c09fe86721ec8125c6225
Ella Zamyatina <ellazamyatina567@gmail.com>
2025-12-20 20:08:55 +0300

    Add method for deleting elem of list, improve main function

--- src/counting/counting.c
+++ src/counting/counting.c
@@ -2,9 +2,20 @@
 #include <stdlib.h>
 
 typedef struct Warrior {
-  int number;
+  int index;
   struct Warrior *next;
 } Warrior;
+
+Warrior* removeWarrior(Warrior *prev, Warrior *current) {
+    if (prev == NULL || current == NULL) {
+        return NULL;
+    }
+    prev->next = current->next;
+    Warrior *nextWarrior = current->next;
+    free(current);
+    return nextWarrior;
+}
+
 
 // implementation of circle(cycle) list
 Warrior *createCircle(int n) {
@@ -12,12 +23,12 @@ Warrior *createCircle(int n) {
     return NULL;
 
   Warrior *first = (Warrior *)malloc(sizeof(Warrior));
-  first->number = 1;
+  first->index = 1;
 
   Warrior *current = first;
   for (int i = 2; i <= n; i++) {
     Warrior *newWarrior = (Warrior *)malloc(sizeof(Warrior));
-    newWarrior->number = i;
+    newWarrior->index = i;
     current->next = newWarrior;
     current = newWarrior;
   }
@@ -48,27 +59,37 @@ int josephus(int n, int m) {
     }
 
     // delete warrior #m
-    prev->next = current->next;
-    Warrior *toDelete = current;
-    current = current->next;
-    free(toDelete);
+    current = removeWarrior(prev,current);
   }
 
-  int survivor = current->number;
+  int survivor = current->index;
   free(current);
   return survivor;
 }
 
 int main() {
-  int n, m;
+  int n = 0;
+  int m = 0;
 
   printf("Введите n: ");
-  scanf("%d", &n);
+  if (scanf("%d", &n) != 1 || n <= 0) {
+      printf("Ошибка, n должно быть положительным числом\n");
+      return 1;
+  }
 
   printf("Введите m: ");
-  scanf("%d", &m);
+  if (scanf("%d", &m) != 1 || m <= 0) {
+      printf("Ошибка, m должно быть положительным числом\n");
+      return 1;
+  }
 
   int survivor = josephus(n, m);
+    
+  if (survivor == -1) {
+      printf("Ошибка, некорректные данные\n");
+      return 1;
+  }
+    
   printf("Выживший воин: %d\n", survivor);
 
   return 0;
