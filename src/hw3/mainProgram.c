6e1b9a45c9b8dbaeeb06a96c8fae26fad95e0042
Ella Zamyatina <ellazamyatina567@gmail.com>
2025-12-20 03:39:07 +0300

    Make WebKit styel

--- src/hw3/mainProgram.c
+++ src/hw3/mainProgram.c
@@ -1,37 +1,38 @@
 #include "sorting.h"
 #include <stdio.h>
 
-int main() {
-  int numbers[100];
-  int count = 0;
+int main()
+{
+    int numbers[100];
+    int count = 0;
 
-  printf("Введите до 100 целых чисел, разделенных пробелами:\n");
+    printf("Введите до 100 целых чисел, разделенных пробелами:\n");
 
-  while (count < 100 && scanf("%d", &numbers[count]) == 1) {
-    count++;
-  }
+    while (count < 100 && scanf("%d", &numbers[count]) == 1) {
+        count++;
+    }
 
-  if (count == 0) {
-    printf("Не введено ни одного числа.\n");
-    return 0;
-  }
+    if (count == 0) {
+        printf("Не введено ни одного числа.\n");
+        return 0;
+    }
 
-  printf("Исходный массив: ");
-  for (int i = 0; i < count; i++) {
-    printf("%d ", numbers[i]);
-  }
-  printf("\n");
+    printf("Исходный массив: ");
+    for (int i = 0; i < count; i++) {
+        printf("%d ", numbers[i]);
+    }
+    printf("\n");
 
-  int countOfMoved = bubbleSort(numbers, count) / 2;
+    int countOfMoved = bubbleSort(numbers, count) / 2;
 
-  printf("Отсортированный массив: ");
-  for (int i = 0; i < count; i++) {
-    printf("%d ", numbers[i]);
-  }
-  printf("\n");
+    printf("Отсортированный массив: ");
+    for (int i = 0; i < count; i++) {
+        printf("%d ", numbers[i]);
+    }
+    printf("\n");
 
-  printf("Количество перемещенных элементов: %d\n", countOfMoved);
-  printf("Количество элементов, участвовавших в сортировке: %d\n", count);
+    printf("Количество перемещенных элементов: %d\n", countOfMoved);
+    printf("Количество элементов, участвовавших в сортировке: %d\n", count);
 
-  return countOfMoved;
+    return countOfMoved;
 }
